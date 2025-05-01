4bf3719877deb997e47403d8a10cb37194ed9c2e
juyeongMoon888 <whoor2828@gmail.com>
2025-05-01 23:57:00 +0900

    feat: Member 객체의 프로퍼티 추가
    Member 객체에 loginId 프로퍼티를 추가.

--- board/src/main/java/com/juyeong/board/domain/Member.java
+++ board/src/main/java/com/juyeong/board/domain/Member.java
@@ -4,19 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
 public class Member {
-
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
 
-    private String name;
+
+    private String loginId; //로그인 아이디
+
+    private String name; //사용자 이름
     private String password;
 }
