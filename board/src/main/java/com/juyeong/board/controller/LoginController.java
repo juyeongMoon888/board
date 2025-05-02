e5d89026919a3845cb07d750a51a75d594fd180a
juyeongMoon888 <whoor2828@gmail.com>
2025-05-02 15:45:26 +0900

    feat: 로그인 검증 추가
    POST로 열어서 로그인 양식 입력 시 글로벌 오류 검증.

--- board/src/main/java/com/juyeong/board/controller/LoginController.java
+++ board/src/main/java/com/juyeong/board/controller/LoginController.java
@@ -1,17 +1,42 @@
 package com.juyeong.board.controller;
 
+import com.juyeong.board.domain.Member;
+import com.juyeong.board.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class LoginController {
+
+    private final LoginService loginService;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute LoginForm form) {
         return "loginForm";
     }
 
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("loginMember={}", loginMember);
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다");
+            return "loginForm";
+        }
 
+
+
+        //로그인 성공 처리
+        return "redirect:/";
+    }
 }
