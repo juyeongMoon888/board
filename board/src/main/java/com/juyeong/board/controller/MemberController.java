dbf11a95644cc4bdf1e5601ab78b04d15201b545
juyeongMoon888 <whoor2828@gmail.com>
2025-05-02 15:55:30 +0900

    feat: 회원가입 시 로그인Id 검증
    로그인Id 필수 입력 검증
    로그인Id 영문 입력 검증
    성공 시 회원에 저장

--- board/src/main/java/com/juyeong/board/controller/MemberController.java
+++ board/src/main/java/com/juyeong/board/controller/MemberController.java
@@ -24,58 +24,32 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/members/new")
-    public String createForm(@ModelAttribute("form") MemberForm memberForm) {
-        return "members/createMemberForm";
+    public String createForm(@ModelAttribute("form") MemberForm form) {
+        return "members/createMemberForm2";
     }
 
-//    @PostMapping("members/new")
-    public String createV1(@ModelAttribute("form") MemberForm form, Model model) {
-
-        //검증 오류 보관
-        Map<String, String> errors = new HashMap<>();
-
-        //검증 로직
-        if (!StringUtils.hasText(form.getName())) {
-            errors.put("name", "아이디는 필수 입니다.");
-        }
-
-        //검증에 실패하면 다시 입력 폼으로
-        if (!errors.isEmpty()) {
-            log.info("errors={}", errors);
-            model.addAttribute("errors", errors);
-            return "/members/createMemberForm";
-        }
-
-        //성공 로직
-        Member member = new Member();
-        member.setName(form.getName());
-        member.setPassword(form.getPassword());
-        memberService.join(member);
-        return "redirect:/";//localhost:8080으로 돌려보낸다. -> 환영합니다 페이지로!
-    }
-
-    @PostMapping("members/new")
+    @PostMapping("/members/new")
     public String createV2(@ModelAttribute("form") MemberForm form, BindingResult bindingResult) {
 
         //검증 로직
-        if (!StringUtils.hasText(form.getName())) {
-            bindingResult.addError(new FieldError("form", "name", "아이디는 필수 입니다."));
+        if (!StringUtils.hasText(form.getLoginId())) {
+            bindingResult.addError(new FieldError("form", "LoginId", "아이디는 필수 입니다."));
         }
         //한글, 정귶
-        if (form.getName().matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) {
-            bindingResult.addError(new FieldError("form", "name", "영문으로 입력하세요"));
+        if (form.getLoginId().matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) {
+            bindingResult.addError(new FieldError("form", "LoginId", "영문으로 입력하세요"));
         }
 
         //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
-            log.info("errors={}", bindingResult);
-            return "/members/createMemberForm2";
+            return "members/createMemberForm2";
         }
 
         //성공 로직
         Member member = new Member();
-        member.setName(form.getName());
+        member.setLoginId(form.getLoginId());
         member.setPassword(form.getPassword());
+        member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";//localhost:8080으로 돌려보낸다. -> 환영합니다 페이지로!
     }
