66b7ade6291c08955fcda158fa7730386dd9c71c
juyeongMoon888 <whoor2828@gmail.com>
2025-05-02 15:51:27 +0900

    feat: findByLoginId 메서드 추가 이외2
    findById(), findAll()

--- board/src/main/java/com/juyeong/board/repository/MemberRepository.java
+++ board/src/main/java/com/juyeong/board/repository/MemberRepository.java
@@ -3,8 +3,12 @@ package com.juyeong.board.repository;
 import com.juyeong.board.domain.Member;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
@@ -13,5 +17,21 @@ public class MemberRepository {
 
     public void save(Member member) {
         em.persist(member);
+    }
+
+    public Member findById(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findByLoginId(String loginId) {
+        return em.createQuery("select m from Member m where m.loginId = :loginId", Member.class)
+                .setParameter("loginId", loginId)
+                .getResultList();
+
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
     }
 }
