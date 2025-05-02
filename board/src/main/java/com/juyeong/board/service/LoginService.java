c89138f686e328c394cd0bc8f9023b0547e21f07
juyeongMoon888 <whoor2828@gmail.com>
2025-05-02 15:47:42 +0900

    feat: 로그인 서비스 생성
    회원 로그인Id로 DB에 저장된 회원을 찾아서 반환.

---
+++ board/src/main/java/com/juyeong/board/service/LoginService.java
@@ -0,0 +1,33 @@
+package com.juyeong.board.service;
+
+import com.juyeong.board.controller.LoginForm;
+import com.juyeong.board.domain.Member;
+import com.juyeong.board.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class LoginService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    public Member login(String loginId, String password) {
+        List<Member> findMember = memberRepository.findByLoginId(loginId);
+        log.info("findMember={}", findMember);
+        return findMember.stream()
+                .filter(m -> m.getPassword().equals(password))
+                .findAny()
+                .orElse(null);
+    }
+
+}
