f1238e98cfb50b912a4b753e797aa0cd14f50125
juyeongMoon888 <whoor2828@gmail.com>
2025-05-03 20:37:58 +0900

    feat: 로그인 유무에 따른 화면 분기
    로그인 한 회원이 아니며, 세션이 없으면 기본 화면으로 이동.

--- board/src/main/java/com/juyeong/board/controller/HomeController.java
+++ board/src/main/java/com/juyeong/board/controller/HomeController.java
@@ -1,20 +1,26 @@
 package com.juyeong.board.controller;
 
+import com.juyeong.board.SessionConst;
+import com.juyeong.board.SessionManager;
 import com.juyeong.board.domain.Member;
 import com.juyeong.board.repository.MemberRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
@@ -22,22 +28,19 @@ public class HomeController {
     private final MemberRepository memberRepository;
 
     @RequestMapping("/")
-    public String homeLogin(
-            @CookieValue(name = "memberId", required = false) Long memberId,
-                            Model model) {
+    public String homeLogin(HttpServletRequest request, Model model) {
 
-        if (memberId == null) {
-            return "home"; //아무것도 받지 않는 html이 필요
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "redirect:/board";
         }
 
-        //로그인 한 사용자 멤버 저장소에서 회원 꺼내기
-        Member loginMember = memberRepository.findById(memberId);
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
         if (loginMember == null) {
-            //쿠키가 너무 오래전에 만들어졌으면 DB에 데이터가 없을 수도 있다.
-            return "home";
+            return "redirect:/board";
         }
+
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
-
 }
